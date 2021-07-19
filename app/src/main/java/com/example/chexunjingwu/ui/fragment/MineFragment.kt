3c5e64d04a4c4de77c54663d6e6824b7642ff45c
lianjf <1548300188@qq.com>
2021-07-19 17:06:18 +0800

    ...

--- app/src/main/java/com/example/chexunjingwu/ui/fragment/MineFragment.kt
+++ app/src/main/java/com/example/chexunjingwu/ui/fragment/MineFragment.kt
@@ -1,11 +1,25 @@
 package com.example.chexunjingwu.ui.fragment
 
+import android.os.Bundle
+import android.view.View
+import com.dylanc.viewbinding.binding
+import com.example.chexunjingwu.BuildConfig
 import com.example.chexunjingwu.R
 import com.example.chexunjingwu.base.BaseFragment
-import com.example.chexunjingwu.base.BaseVMFragment
+import com.example.chexunjingwu.databinding.FragmentMineBinding
+import com.example.chexunjingwu.http.response.HomeLoginResponse
+import com.example.chexunjingwu.tools.DataHelper
 
 class MineFragment : BaseFragment(R.layout.fragment_mine) {
+    val bind: FragmentMineBinding by binding()
+    var userInfo: HomeLoginResponse.Data.Data.User =
+        DataHelper.getData(DataHelper.loginUserInfo) as HomeLoginResponse.Data.Data.User;
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bind.tvName.text = "姓名:${userInfo.name}"
+        bind.tvJh.text = "警号:${userInfo.pcard}"
+        bind.tvVersionName.text = "版本:${BuildConfig.VERSION_NAME}"
+    }
 
 }
